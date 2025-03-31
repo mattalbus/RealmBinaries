26f5d02e055428e45eff4c037d1a2485cbda5c95
matthieu <60342216+mattalbus@users.noreply.github.com>
2025-03-31 23:06:00 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,13 +8,8 @@ func buildTargets() -> [Target] {
     let realmUrl = "https://github.com/mattalbus/RealmBinaries/releases/download/\(realmVersion)/Realm.spm.zip"
     let realmChecksum = "038388b2f5a9e440b6fe173ac5951f9370edc3c039268856325ca8c41115430b"
 
-#if swift(>=6.1)
-    let realmSwiftUrl = "https://github.com/realm/realm-swift/releases/download/v\(realmVersion)/RealmSwift@16.3.spm.zip"
+    let realmSwiftUrl = "https://github.com/realm/realm-swift/releases/download/v\(realmVersion)/RealmSwift-16.3.spm.zip"
     let realmSwiftChecksum = "cc0c4b32cdec945593b53bd1c80770ec2b4dbe4385efbb864e9dd0691deb4fc5"
-#else
-    let realmSwiftUrl = "https://github.com/realm/realm-swift/releases/download/v\(realmVersion)/RealmSwift@16.1.spm.zip"
-    let realmSwiftChecksum = "41e068d62364282962472c7e93aa936de1da88b8faaf8a81b97700d5e8a499d2"
-#endif
     
     return [
         .binaryTarget(
