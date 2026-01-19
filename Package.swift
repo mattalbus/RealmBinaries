745bae2d89a613be385f5c2546a080a3fa08c2ce
matthieu <60342216+mattalbus@users.noreply.github.com>
2026-01-19 14:04:00 +0100

    downgrade swift tools version

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:6.2
+// swift-tools-version:5.10
 
 import PackageDescription
 
 let realmVersion = "20.0.3"
-let sofiaVersion = "2003.0.5"
+let sofiaVersion = "2003.0.6"
 
 func buildTargets() -> [Target] {
     let realmUrl = "https://github.com/mattalbus/RealmBinaries/releases/download/\(sofiaVersion)/Realm.spm.zip"
