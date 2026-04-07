bcf385b4327ee780221505fe58c7eeda5461ceb3
matthieu <60342216+mattalbus@users.noreply.github.com>
2026-04-07 13:55:29 +0200

    fix checksums

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@
 import PackageDescription
 
 let realmVersion = "20.0.4"
-let sofiaVersion = "2004.0.1"
+let sofiaVersion = "2004.0.2"
 
 func buildTargets() -> [Target] {
     let realmUrl = "https://github.com/mattalbus/RealmBinaries/releases/download/\(sofiaVersion)/Realm.spm.zip"
-    let realmChecksum = "ec7e2fe777d38cd06205d82ba9ade8a29ec9a7187731b8b1327c704023dadb9b"
+    let realmChecksum = "32d4878102cb3a5510078141b85f74dd2893fc33733a14cf9b2f429502006373"
 
     // Xcode 26.4
     let realmSwiftUrl = "https://github.com/mattalbus/RealmBinaries/releases/download/\(sofiaVersion)/RealmSwift.spm.zip"
-    let realmSwiftChecksum = "2d72ace26fb19bb8f12af34e7b53e42d4e130091677dbccef2a14bd4b4f5339c"
+    let realmSwiftChecksum = "38384ac031f547b52d49f29e74e52c13fabd442e7e5a6c216f1fb5c4a9492bfc"
     
     return [
         .binaryTarget(
