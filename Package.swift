6448cfd8d0d7dcb48a6ce51b647d8c6f0bc188d9
matthieu <60342216+mattalbus@users.noreply.github.com>
2026-04-27 12:10:37 +0200

    support xcode 26.4.1

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@
 import PackageDescription
 
 let realmVersion = "20.0.4"
-let sofiaVersion = "2004.0.2"
+let sofiaVersion = "2004.0.3"
 
 func buildTargets() -> [Target] {
     let realmUrl = "https://github.com/mattalbus/RealmBinaries/releases/download/\(sofiaVersion)/Realm.spm.zip"
-    let realmChecksum = "32d4878102cb3a5510078141b85f74dd2893fc33733a14cf9b2f429502006373"
+    let realmChecksum = "676ae3366d9e368a51d930a271d3dfeaac7ac29ba13b3cad412912d49bbb9282"
 
-    // Xcode 26.4
+    // Xcode 26.4.1
     let realmSwiftUrl = "https://github.com/mattalbus/RealmBinaries/releases/download/\(sofiaVersion)/RealmSwift.spm.zip"
-    let realmSwiftChecksum = "38384ac031f547b52d49f29e74e52c13fabd442e7e5a6c216f1fb5c4a9492bfc"
+    let realmSwiftChecksum = "8b541cf836505a6cfdeb9ed51b1aa7715249f6f0b455db4d33144e770e3916b3"
     
     return [
         .binaryTarget(
